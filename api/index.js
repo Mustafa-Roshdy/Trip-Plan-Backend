db85c47cf2571c690efe8cdea3e73b5b4851b519
Mustafa Mohamed Roshdy <120448988+Mustafa-Roshdy@users.noreply.github.com>
2025-12-01 12:46:44 +0200

    updated index.js

--- api/index.js
+++ api/index.js
@@ -24,17 +24,17 @@ const allowedOrigins = [
   "http://127.0.0.1:5500",
 ];
 
-app.use(
-  cors({
-    origin: function (origin, callback) {
-      if (!origin || allowedOrigins.includes(origin)) {
-        return callback(null, true);
-      }
-      return callback(new Error("Not allowed by CORS"));
-    },
-    credentials: true,
-  })
-);
+app.use()
+  // cors({
+  //   origin: function (origin, callback) {
+  //     if (!origin || allowedOrigins.includes(origin)) {
+  //       return callback(null, true);
+  //     }
+  //     return callback(new Error("Not allowed by CORS"));
+  //   },
+  //   credentials: true,
+  // })
+// );
 // Middlewares
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
